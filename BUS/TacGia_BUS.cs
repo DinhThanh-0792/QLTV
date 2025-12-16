dfc5887805997cb4202d56bb3d1b8126061d67b5
DinhThanh-0792 <danhconmeo225@gmail.com>
2025-12-16 08:39:40 +0700

    Add files via upload

---
+++ BUS/TacGia_BUS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+using System.Data;
+
+namespace BUS
+{
+    public class TacGia_BUS
+    {
+        TacGia_DAL dAL = new TacGia_DAL();
+        public DataTable LoadTacGia()
+        {
+            return dAL.LoadTacGia();
+        }
+        public void InsertTacGia(TacGia tg)
+        {
+            dAL.InsertTacGia(tg);
+        }
+        public void UpdateTacGia(TacGia tg)
+        {
+            dAL.UpdateTacGia(tg);
+        }
+        public void DeleteTacGia(string maTG)
+        {
+            dAL.DeleteTacGia(maTG);
+        }
+    }
+}
